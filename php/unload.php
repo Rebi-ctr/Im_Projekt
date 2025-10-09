7d862cf6ec3806f6dccdd22d2c3400190839c7e5
Mathis Tobler <mathis.tobler@stud.fhgr.ch>
2025-10-09 15:21:16 +0200

    php unload

--- php/unload.php
+++ php/unload.php
@@ -1,6 +1,6 @@
 <?php
 
-header('Access-Control-Allow-Origin: https://im3.mathis-tobler.ch/');
+header('Access-Control-Allow-Origin: https://im3.mathis-tobler.ch');
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS'); 
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
